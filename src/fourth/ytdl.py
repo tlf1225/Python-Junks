5dd311fad4e8f9f92cd8466052ea672140063b41
tlf <42900257+tlf1225@users.noreply.github.com>
2020-06-05 18:44:50 +0900

    3rd Update

--- src/fourth/ytdl.py
+++ src/fourth/ytdl.py
@@ -1,17 +1,27 @@
 from youtube_dl import YoutubeDL
-from getpass import getpass
 
 
 def main():
     ytdl = YoutubeDL(params={
-        "username": "bulemasa5727",
-        "password": getpass("Password:"),
         "no_cache": True,
         "no_warnings": False,
         "ignoreerrors": True,
-        "simulate": True
+        "quiet": True,
+        "skip_download": True,
+        "simulate": True,
+        "ffmpeg_location": "D:/ffmpeg-20200603-b6d7c4c-win64-shared/bin"
     })
-    ytdl.extract_info(url="https://www.youtube.com/playlist?list=PLfwcn8kB8EmMQSt88kswhY-QqJtWfVYEr", download=False)
+    result = ytdl.extract_info(url="https://www.youtube.com/playlist?list=PLfwcn8kB8EmMQSt88kswhY-QqJtWfVYEr", download=False)
+    if "entries" not in result:
+        print(result["title"])
+        for url in sorted(result["formats"], key=lambda x: int(x["format_id"])):
+            print(f"{url['format']} {url['url']}")
+    else:
+        for entries in result["entries"]:
+            print(entries["title"])
+            for url in sorted(entries["formats"], key=lambda x: int(x["format_id"])):
+                print(f"{url['format']} {url['url']}")
+            print()
 
 
 if __name__ == '__main__':
