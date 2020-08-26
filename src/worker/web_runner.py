26b9aade679b46e5a4fbb9c79b5007f68c807585
tlf <42900257+tlf1225@users.noreply.github.com>
2020-08-26 14:33:15 +0900

    Minimal

--- src/worker/web_runner.py
+++ src/worker/web_runner.py
@@ -1,12 +1,12 @@
 # import threading
 # from subprocess import call
-from concurrent.futures import ProcessPoolExecutor
-from concurrent.futures.process import BrokenProcessPool
+from concurrent.futures import ThreadPoolExecutor
 from http.server import ThreadingHTTPServer, CGIHTTPRequestHandler
 from logging import basicConfig, error, info, DEBUG
 from mimetypes import add_type
 from os import chdir
-from ssl import create_default_context, Purpose
+from ssl import create_default_context, wrap_socket, Purpose
+from sys import stderr
 
 # noinspection PyUnresolvedReferences
 from typing import Tuple, Optional, Union
@@ -16,14 +16,15 @@ FORMAT = "%(levelname)s: %(asctime)s [%(funcName)s] {%(threadName)s:%(thread)d}:
 
 class HTTPWorker(CGIHTTPRequestHandler):
 
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
+    def setup(self):
+        super().setup()
+        self.cgi_directories += ["/2/cgi-bin"]
 
-    def handle_one_request(self):
-        try:
-            super(HTTPWorker, self).handle_one_request()
-        except OSError as e:
-            print(e)
+    def log_message(self, fmt: str, *args) -> None:
+        print(f"{self.address_string()} [{self.log_date_time_string()}] {fmt % args}", file=stderr)
+
+    def do_GET(self) -> None:
+        super().do_GET()
 
 
 def http(root=None):
@@ -39,12 +40,12 @@ def https(root=None):
     ssl = create_default_context(Purpose.CLIENT_AUTH)
     ssl.load_cert_chain(certfile="../certificate.crt", keyfile="../private.key")
     ssl.load_verify_locations(cafile="../ca_bundle.crt")
-    ssl.set_alpn_protocols(["http/1.1"])
+    ssl.set_alpn_protocols(["http/1.1", "h2", "h3"])
     chdir(root)
     basicConfig(level=DEBUG, format=FORMAT)
     with ThreadingHTTPServer(("localhost", 443), HTTPWorker) as httpd:
         info("Running")
-        httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True)
+        httpd.socket = wrap_socket(httpd.socket, server_side=True)
         httpd.serve_forever()
         error("Exit")
 
@@ -52,14 +53,14 @@ def https(root=None):
 def main():
     chdir("root")
     add_type("text/javascript", ".js")
-    with ProcessPoolExecutor(max_workers=5) as worker:
+    with ThreadPoolExecutor(max_workers=5) as worker:
         root = "."
         a = [worker.submit(http, root=root),
              worker.submit(https, root=root)]
         for c in a:
             try:
                 c.exception()
-            except (FileNotFoundError, KeyboardInterrupt, BrokenProcessPool):
+            except (FileNotFoundError, KeyboardInterrupt):
                 print("Cancelling")
                 c.cancel()
 
