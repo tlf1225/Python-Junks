f981744162ddd04fa79092b2da85dec5175ced7d
tlf1225 <42900257+tlf1225@users.noreply.github.com>
2021-01-03 11:26:05 +0000

    Optimize

--- src/tlf1225/enum_windows.py
+++ src/tlf1225/enum_windows.py
@@ -3,41 +3,54 @@ from pywintypes import error as win_exception
 # noinspection PyUnresolvedReferences
 from win32con import HWND_TOPMOST, HWND_NOTOPMOST, SWP_NOMOVE, SWP_NOSIZE
 from win32gui import FindWindow, FindWindowEx, EnumWindows, EnumChildWindows, GetClassName, GetWindowText, SendMessageTimeout, SetWindowPos, \
-    SetForegroundWindow
+    SetForegroundWindow, EnumThreadWindows
 from win32process import GetCurrentProcessId, GetWindowThreadProcessId
 
 
-def search_background():
-    result = ()
+def enum_window():
     try:
-        def test(a, b):
-            nonlocal result
-            cls = GetClassName(a)
-            wt = GetWindowText(a)
-            print("\t" * b + f"H: {a}, C: {cls}, T: {wt}")
-            if cls == "WorkerW":
-                shell = FindWindowEx(a, None, "SHELLDLL_DefView", None)
-                if shell:
-                    view = FindWindowEx(shell, None, "SysListView32", "FolderView")
-                    if view:
-                        worker = FindWindowEx(None, a, "WorkerW", None)
-                        result = (cls, wt, a, shell, view, worker)
-                        return False
-            return EnumChildWindows(a, test, b + 1)
+        def test(hwnd, param):
+            cls, wt = GetClassName(hwnd), GetWindowText(hwnd)
+            print("\t" * param + f"H: {hwnd}, C: {cls}, T: {wt}")
+            return EnumChildWindows(hwnd, test, param + 1)
 
         EnumWindows(test, 0)
     except win_exception as e:
         print(e.strerror)
 
-    return result
 
-
-def default_background():
+def default_info():
     pro = FindWindow("Progman", "Program Manager")
     shell = FindWindowEx(pro, None, "SHELLDLL_DefView", None)
     view = FindWindowEx(shell, None, "SysListView32", "FolderView")
+    return pro, shell, view
+
+
+def search_background():
+    pro = FindWindow("Progman", "Program Manager")
     SendMessageTimeout(pro, 0x52C, None, None, 0, 1000)
-    return view
+    tid, pid = GetWindowThreadProcessId(pro)
+    result = None
+    try:
+        def test(hwnd, param):
+            nonlocal result
+            cls, wt = GetClassName(hwnd), GetWindowText(hwnd)
+            print("\t" * param + f"H: {hwnd}, C: {cls}, T: {wt}")
+            if cls == "WorkerW":
+                shell = FindWindowEx(hwnd, None, "SHELLDLL_DefView", None)
+                if shell:
+                    view = FindWindowEx(shell, None, "SysListView32", "FolderView")
+                    if view:
+                        worker = FindWindowEx(None, hwnd, "WorkerW", None)
+                        result = pid, tid, cls, wt, hwnd, shell, view, worker
+                        return False
+            return True
+
+        EnumThreadWindows(tid, test, 0)
+    except win_exception as e:
+        print(e.strerror)
+
+    return result
 
 
 # noinspection PyTypeChecker, SpellCheckingInspection
@@ -45,12 +58,13 @@ def topmost(hwnd_value):
     def finder(hwnd, lp):
         tid, pid = GetWindowThreadProcessId(hwnd)
         if pid == lp:
-            SetWindowPos(hwnd, hwnd_value, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE)
             SetForegroundWindow(hwnd)
+            SetWindowPos(hwnd, hwnd_value, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE)
 
     EnumWindows(finder, GetCurrentProcessId())
 
 
 if __name__ == '__main__':
-    print(default_background())
+    # print(default_info())
+    # print(enum_window())
     print(search_background())
