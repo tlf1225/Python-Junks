fd937c8723a5ee37fe35ffd06d94b7b1356afe3b
tlf1225 <42900257+tlf1225@users.noreply.github.com>
2021-01-08 06:35:00 +0000

    Improve Topmost Method

--- src/tlf1225/mpv/back.py
+++ src/tlf1225/mpv/back.py
@@ -8,7 +8,7 @@ from pywintypes import error as win_exception
 from win32con import HWND_TOPMOST, HWND_NOTOPMOST, SWP_NOMOVE, SWP_NOSIZE
 from win32gui import FindWindow, FindWindowEx, EnumWindows, EnumChildWindows, GetClassName, GetWindowText, SendMessageTimeout, SetWindowPos, \
     SetForegroundWindow, EnumThreadWindows, GetParent
-from win32process import GetCurrentProcessId, GetWindowThreadProcessId
+from win32process import GetWindowThreadProcessId
 
 
 def enum_parent(hwnd: int, param: list):
@@ -39,7 +39,7 @@ def enum_windows():
                 result.append((i, tuple(child)))
             else:
                 result.append(i)
-        return result
+        return tuple(result)
     except win_exception as e:
         print(e.strerror)
 
@@ -56,52 +56,40 @@ def search_background():
     pro = FindWindow("Progman", "Program Manager")
     SendMessageTimeout(pro, 0x52C, None, None, 0, 1000)
     tid, pid = GetWindowThreadProcessId(pro)
-    result = ()
+    result = [(pid, tid), []]
     try:
-        def worker(hwnd: int, param):
-            nonlocal result
-            cls, wt = GetClassName(hwnd), GetWindowText(hwnd)
-            print("\t" * param + f"H: {hwnd}, C: {cls}, T: {wt}", file=stderr)
-            if cls == "WorkerW":
-                shell = FindWindowEx(hwnd, None, "SHELLDLL_DefView", None)
-                if shell:
-                    view = FindWindowEx(shell, None, "SysListView32", "FolderView")
-                    if view:
-                        window = FindWindowEx(None, hwnd, "WorkerW", None)
-                        result = pid, tid, cls, wt, hwnd, shell, view, window
-                        return False
-            return True
-
-        EnumThreadWindows(tid, worker, 0)
+        EnumThreadWindows(tid, enum_child, (-1, result[1]))
     except win_exception as e:
         print(e.strerror)
 
     return result
 
 
-# noinspection PyTypeChecker, SpellCheckingInspection
-def topmost(hwnd_value: int):
-    def finder(hwnd: int, lp):
-        nonlocal hwnd_value
-        tid, pid = GetWindowThreadProcessId(hwnd)
-        if pid == lp:
-            SetForegroundWindow(hwnd)
-            SetWindowPos(hwnd, hwnd_value, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE)
-
-    EnumWindows(finder, GetCurrentProcessId())
-
-
-def show_enum_windows(enum=None, c=0):
+# noinspection SpellCheckingInspection
+def topmost(enum=enum_windows(), c=0, hwnd_value: int = -1, cl: str = None, wtx: str = None):
     for i in enum:
         if isinstance(i, tuple):
             show_enum_windows(i, c + 1)
         else:
-            print('\t' * (c - 1) + f"H: {i}, C: {GetClassName(i)}, T: {GetWindowText(i)}")
+            cls, wt = GetClassName(i), GetWindowText(i)
+            if cls == cl or wt == wtx:
+                SetForegroundWindow(i)
+                SetWindowPos(i, hwnd_value, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE)
+                break
+    else:
+        print("Not Found", file=stderr)
+
+
+def show_enum_windows(enum=enum_windows(), c=0):
+    for i in enum:
+        if isinstance(i, tuple):
+            show_enum_windows(i, c + 1)
+        else:
+            print('\t' * (c - 1) + f"H: {i}, C: {GetClassName(i)}, T: {GetWindowText(i)}", file=stderr)
+    else:
+        print("That's all", file=stderr)
 
 
 if __name__ == '__main__':
-    # print(default_info())
-    show_enum_windows(enum_windows())
-    res = search_background()
-    print(res)
-    print(FindWindowEx(res[7], None, "mpv", None))
+    show_enum_windows()
+    print(search_background(), file=stderr)
