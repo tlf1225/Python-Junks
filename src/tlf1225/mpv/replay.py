5accfadbce1d4561cf581a7f7d9763539a97e9f8
tlf1225 <42900257+tlf1225@users.noreply.github.com>
2021-01-20 10:02:29 +0000

    Test

--- src/tlf1225/mpv/replay.py
+++ src/tlf1225/mpv/replay.py
@@ -10,13 +10,29 @@ from remake import *
 def main():
     mpv_handle = create()
 
+    request_log_messages(mpv_handle, b"warn")
     set_option_string(mpv_handle, b"input-vo-keyboard", b"yes")
     set_option_string(mpv_handle, b"input-default-bindings", b"yes")
     set_option_string(mpv_handle, b"input-media-keys", b"yes")
     set_option_string(mpv_handle, b"osc", b"yes")
-    set_option_string(mpv_handle, b"ytdl-raw-options", b"no-cache-dir=")
     set_option_string(mpv_handle, b"shuffle", b"yes")
+    set_option_string(mpv_handle, b"vo", b"gpu,direct3d,sdl")
+    set_option_string(mpv_handle, b"ao", b"wasapi,openal,sdl")
+    set_option_string(mpv_handle, b"hwdec", b"auto-copy-safe")
+    set_option_string(mpv_handle, b"gpu-api", b"auto")
+    set_option_string(mpv_handle, b"loop-playlist", b"inf")
+    set_option_string(mpv_handle, b"volume-max", b"100")
+    set_option_string(mpv_handle, b"input-ipc-server", rb"\\.\pipe\tlf1225")
+    set_option_string(mpv_handle, b"autofit", b"1280x720")
+    set_option_string(mpv_handle, b"gemetry", b"1280x720")
+    set_option_string(mpv_handle, b"ytdl-format", b"bestvideo+bestaudio/best")
+    set_option_string(mpv_handle, b"ytdl-raw-options", b"no-cache-dir=")
     set_option_string(mpv_handle, b"wid", str(search_background()[1][6]).encode())
+    set_option_string(mpv_handle, b"af", b"@default:lavfi=[dynaudnorm=b=1:c=1:g=11:r=1.0],asoftclip=type=tanh")
+    set_option_string(mpv_handle, b"vf", b"@default:lavfi=[fade=in:0:60,pad=0:ih+80:-1:-1:0x008fbf:0:16/9]")
+
+    load_config_file(mpv_handle, b"mpv.conf")
+
     # set_option_string(mpv_handle, b"audio-display", b"yes")
 
     initialize(mpv_handle)
@@ -35,6 +51,44 @@ def main():
     test = (c_char_p * 2)(b"loadfile", b"ytdl://PLfwcn8kB8EmMQSt88kswhY-QqJtWfVYEr")
 
     command(mpv_handle, test)
+
+    command(mpv_handle, c_char_p(b"playlist-shuffle"))
+
+    """
+    from ctypes import windll, byref
+    from json import loads
+    
+    command(mpv_handle, (c_char_p * 2)(b"keypress", b"i"))
+    command(mpv_handle, c_char_p(b"playlist-next"))
+    command(mpv_handle, c_char_p(b"playlist-shuffle"))
+    command(mpv_handle, (c_char_p * 2)(b"cycle", "pause"))
+    
+    x = c_int64()
+    get_property(mpv_handle, b"duration", 4, byref(x))
+    set_property(mpv_handle, b"playlist-pos", 4, byref(x))
+    set_property(mpv_handle, b"playlist-pos", 4, byref(c_int(0)))
+    
+    y = loads(get_property_string(mpv_handle, b"playlist"))
+
+    get_property_string(mpv_handle, b"wid")    
+    set_property_string(mpv_handle, b"loop", b"inf")
+    set_property_string(mpv_handle, b"speed", b"1.25")
+    set_property_string(mpv_handle, b"playlist-pos", b"22")
+    
+    for i in playlist:
+        command(mpv_handle, (c_char_p * 4)(b"loadfile", i.encode(), b"append"))
+    
+    command_string(mpv_handle, b"seek 120")
+    command_string(mpv_handle, b"keypress i")
+    command_string(mpv_handle, b"playlist-next")
+    
+    windll.kernel32.SetConsoleTitleA(get_property_string(mpv_handle, b"media-title"))
+    
+    frame = c_int64()
+    get_property(mpv_handle, b"estimated-frame-count", 4, byref(frame))
+    command(mpv_handle, (c_char_p * 3)(b"vf", b"toggle", f"@temp:lavfi=[fade=out:{frame.value}:60]".encode()))
+    command(mpv_handle, (c_char_p * 3)(b"vf", b"remove", b"@temp"))
+    """
 
     flag = 0
 
