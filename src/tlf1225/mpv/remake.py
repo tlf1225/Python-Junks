3e1f27335499cb35fd817e4dff8b61f959030125
tlf1225 <42900257+tlf1225@users.noreply.github.com>
2021-01-20 03:46:14 +0000

    Test

--- src/tlf1225/mpv/remake.py
+++ src/tlf1225/mpv/remake.py
@@ -1,7 +1,5 @@
-from code import interact
-from ctypes import CDLL, c_void_p, c_char_p, c_int, c_int64, c_uint64, c_ulong, c_double, CFUNCTYPE, POINTER, create_string_buffer, sizeof, cast
+from ctypes import CDLL, c_void_p, c_char_p, c_int, c_int64, c_uint64, c_ulong, c_double, CFUNCTYPE, POINTER
 from ctypes.util import find_library
-from threading import Thread
 
 mpv = CDLL(find_library("mpv-1.dll"))
 
@@ -57,7 +55,7 @@ get_opt_a = con_tx + ((1, "reply_data"), (1, "name"), (1, "format"))
 set_opt_a = get_opt_a + ((1, "data"),)
 
 ob_opt = con_tx + ((1, "reply_data"), (1, "name"), (1, "format"))
-unob_opt = con_tx + ((1, "registered_reply_userdata"),)
+u_ob_opt = con_tx + ((1, "registered_reply_userdata"),)
 
 ev = (1, "event"),
 en = (1, "dst"), (1, "src")
@@ -73,65 +71,117 @@ ctc = (2, "res"), (1, "mpv"), (1, "params")
 cts = (1, "ctx"), (1, "param")
 csc = (1, "ctx"), (1, "callback"), (1, "callback_ctx")
 
+# noinspection PyArgumentList
 client_api_version = ul(("mpv_client_api_version", mpv), None)
+# noinspection PyArgumentList
 error_string = cp_i(("mpv_error_string", mpv), err)
+# noinspection PyArgumentList
 free = v_vp(("mpv_free", mpv), dat)
 
+# noinspection PyArgumentList
 client_name = cp_vp(("mpv_client_name", mpv), con_tx)
+# noinspection PyArgumentList
 client_id = i64_vp(("mpv_client_id", mpv), con_tx)
 
+# noinspection PyArgumentList
 create = vp(("mpv_create", mpv), None)
+# noinspection PyArgumentList
 initialize = i_vp(("mpv_initialize", mpv), con_tx)
+# noinspection PyArgumentList
 destroy = v_vp(("mpv_destroy", mpv), con_tx)
+# noinspection PyArgumentList
 terminate_destroy = v_vp(("mpv_terminate_destroy", mpv), con_tx)
 
+# noinspection PyArgumentList
 create_client = vp_vp_cp(("mpv_create_client", mpv), cre_cli_p)
+# noinspection PyArgumentList
 create_weak_client = vp_vp_cp(("mpv_create_weak_client", mpv), cre_cli_p)
-
+# noinspection PyArgumentList
 load_config_file = i_vp_cp(("mpv_load_config_file", mpv), ld_conf_p)
+# noinspection PyArgumentList
 get_time_us = i64_vp(("mpv_get_time_us", mpv), con_tx)
+# noinspection PyArgumentList
 free_node_contents = v_vp(("mpv_free_node_contents", mpv), nd)
 
+# noinspection PyArgumentList
 set_option = i_vp_cp_i_vp(("mpv_set_option", mpv), so)
+# noinspection PyArgumentList
 set_option_string = i_vp_cp_cp(("mpv_set_option_string", mpv), set_opt_p)
 
+# noinspection PyArgumentList
 command = i_vp_cpp(("mpv_command", mpv), cmd_p)
+# noinspection PyArgumentList
 command_node = i_vp_vp_vp(("mpv_command_node", mpv), cm_nd)
+# noinspection PyArgumentList
 command_ret = i_vp_vpp_vp(("mpv_command_ret", mpv), cm_nd)
+# noinspection PyArgumentList
 command_string = i_vp_cp(("mpv_command_string", mpv), cmd_p)
+# noinspection PyArgumentList
 command_async = i_vp_ui64_cpp(("mpv_command_async", mpv), cm_as)
+# noinspection PyArgumentList
 command_node_async = i_vp_ui64_vp(("mpv_command_node_async", mpv), cm_as)
+# noinspection PyArgumentList
 abort_async_command = v_vp_ui64(("mpv_abort_async_command", mpv), ab_cm)
 
+# noinspection PyArgumentList
 set_property = i_vp_cp_vp_vp(("mpv_set_property", mpv), set_opt)
+# noinspection PyArgumentList
 set_property_string = i_vp_cp_cp(("mpv_set_property_string", mpv), set_opt_p)
+# noinspection PyArgumentList
 set_property_async = i_vp_ui64_cp_vp_vp(("mpv_set_property_async", mpv), set_opt_a)
+# noinspection PyArgumentList
 get_property = i_vp_cp_vp_vp(("mpv_get_property", mpv), get_opt)
+# noinspection PyArgumentList
 get_property_string = cp_vp_cp(("mpv_get_property_string", mpv), get_opt_p)
+# noinspection PyArgumentList
 get_property_osd_string = cp_vp_cp(("mpv_get_property_osd_string", mpv), get_opt_p)
+# noinspection PyArgumentList
 get_property_async = i_vp_ui64_cp_vp(("mpv_get_property_async", mpv), get_opt_a)
+# noinspection PyArgumentList
 observe_property = i_vp_ui64_cp_vp(("mpv_observe_property", mpv), ob_opt)
-unobserve_property = i_vp_ui64(("mpv_unobserve_property", mpv), unob_opt)
+# noinspection PyArgumentList, SpellCheckingInspection
+unobserve_property = i_vp_ui64(("mpv_unobserve_property", mpv), u_ob_opt)
+
+# noinspection PyArgumentList
 event_name = cp_i(("mpv_event_name", mpv), ev)
+# noinspection PyArgumentList
 event_to_node = i_vp_vp(("mpv_event_to_node", mpv), en)
+# noinspection PyArgumentList
 request_event = i_vp_i_i(("mpv_request_event", mpv), re)
+# noinspection PyArgumentList
 request_log_messages = i_vp_cp(("mpv_request_log_messages", mpv), rl)
+# noinspection PyArgumentList
 wait_event = vp_vp_d(("mpv_wait_event", mpv), we)
+# noinspection PyArgumentList
 wakeup = v_vp(("mpv_wakeup", mpv), con_tx)
+# noinspection PyArgumentList
 set_wakeup_callback = v_vp_f_vp(("mpv_set_wakeup_callback", mpv), swc)
+# noinspection PyArgumentList
 wait_async_requests = v_vp(("mpv_wait_async_requests", mpv), con_tx)
+
+# noinspection PyArgumentList
 hook_add = i_vp_ui64_cp_i(("mpv_hook_add", mpv), hk)
+# noinspection PyArgumentList
 hook_continue = i_vp_ui64(("mpv_hook_continue", mpv), hk_c)
 
+# noinspection PyArgumentList
 render_context_create = i_vpp_vp_vp(("mpv_render_context_create", mpv), ctc)
+# noinspection PyArgumentList
 render_context_set_parameter = i_vp_vp(("mpv_render_context_set_parameter", mpv), cts)
+# noinspection PyArgumentList
 render_context_get_info = i_vp_vp(("mpv_render_context_get_info", mpv), cts)
+# noinspection PyArgumentList
 render_context_set_update_callback = v_vp_f_vp(("mpv_render_context_set_update_callback", mpv), csc)
+# noinspection PyArgumentList
 render_context_update = ui64_vp(("mpv_render_context_update", mpv), con_tx)
+# noinspection PyArgumentList
 render_context_render = i_vp_vp(("mpv_render_context_render", mpv), cts)
+# noinspection PyArgumentList
 render_context_report_swap = v_vp(("mpv_render_context_report_swap", mpv), con_tx)
+# noinspection PyArgumentList
 render_context_free = v_vp(("mpv_render_context_free", mpv), con_tx)
 
+# noinspection SpellCheckingInspection
 __all__ = ["client_api_version", "error_string", "free", "client_name", "client_id", "create", "initialize", "destroy", "terminate_destroy",
            "create_client", "create_weak_client", "load_config_file", "get_time_us", "free_node_contents", "set_option", "set_option_string",
            "command", "command_node", "command_ret", "command_string", "command_async", "command_node_async", "abort_async_command", "set_property",
@@ -140,72 +190,3 @@ __all__ = ["client_api_version", "error_string", "free", "client_name", "client_
            "set_wakeup_callback", "wait_async_requests", "hook_add", "hook_continue", "render_context_create", "render_context_set_parameter",
            "render_context_set_update_callback", "render_context_update", "render_context_render", "render_context_report_swap",
            "render_context_free"]
-
-if __name__ == '__main__':
-    mpv_handle = create()
-
-    set_option_string(mpv_handle, b"input-vo-keyboard", b"yes")
-    set_option_string(mpv_handle, b"input-default-bindings", b"yes")
-    set_option_string(mpv_handle, b"input-media-keys", b"yes")
-    set_option_string(mpv_handle, b"osc", b"yes")
-    set_option_string(mpv_handle, b"ytdl-raw-options", b"no-cache-dir=")
-    set_option_string(mpv_handle, b"shuffle", b"yes")
-    # set_option_string(mpv_handle, b"audio-display", b"yes")
-
-    initialize(mpv_handle)
-
-    host_name = client_name(mpv_handle)
-    host_id = client_id(mpv_handle)
-
-    mpv_client_handle = create_client(mpv_handle, b"Worker")
-
-    client_name = client_name(mpv_client_handle)
-    client_id = client_id(mpv_client_handle)
-
-    print(host_name, client_name)
-    print(host_id, client_id)
-
-    test = (c_char_p * 2)(cast(create_string_buffer(64), c_char_p), cast(create_string_buffer(64), c_char_p))
-    test[0] = b"loadfile"
-    test[1] = b"ytdl://PLfwcn8kB8EmMQSt88kswhY-QqJtWfVYEr"
-
-    command(mpv_handle, test)
-
-
-    def check_shutdown():
-        while True:
-            ad = wait_event(mpv_client_handle, -1)
-            off = ad
-            x = c_int.from_address(off).value
-            off += sizeof(c_int)
-            y = c_int.from_address(off).value
-            off += sizeof(c_int)
-            z = c_int64.from_address(off).value
-            off += sizeof(c_uint64)
-            a = c_void_p.from_address(off).value
-            print(ad, x, y, z, a)
-            if x == 1:
-                break
-
-
-    th = Thread(target=check_shutdown)
-
-    th.start()
-
-    while th.is_alive():
-        try:
-            interact(banner="Mpv Player", local=locals(), exitmsg="Exit")
-        except SystemExit as ex:
-            if ex.code:
-                break
-        except Exception as e:
-            print(e)
-        wakeup(mpv_client_handle)
-
-    command(mpv_handle, (c_char_p * 1)(c_char_p(b"quit")))
-
-    th.join(3)
-
-    destroy(mpv_client_handle)
-
-    terminate_destroy(mpv_handle)
---
+++ src/tlf1225/mpv/replay.py
@@ -0,0 +1,77 @@
+from code import interact
+from ctypes import c_void_p, c_char_p, c_int, c_int64, c_uint64, create_string_buffer, sizeof, cast
+from threading import Thread
+
+from remake import *
+
+
+# noinspection SpellCheckingInspection
+def main():
+    mpv_handle = create()
+
+    set_option_string(mpv_handle, b"input-vo-keyboard", b"yes")
+    set_option_string(mpv_handle, b"input-default-bindings", b"yes")
+    set_option_string(mpv_handle, b"input-media-keys", b"yes")
+    set_option_string(mpv_handle, b"osc", b"yes")
+    set_option_string(mpv_handle, b"ytdl-raw-options", b"no-cache-dir=")
+    set_option_string(mpv_handle, b"shuffle", b"yes")
+    # set_option_string(mpv_handle, b"audio-display", b"yes")
+
+    initialize(mpv_handle)
+
+    ho_n = client_name(mpv_handle)
+    ho_i = client_id(mpv_handle)
+
+    mpv_client_handle = create_client(mpv_handle, b"Worker")
+
+    cl_n = client_name(mpv_client_handle)
+    cl_i = client_id(mpv_client_handle)
+
+    print(ho_n, cl_n)
+    print(ho_i, cl_i)
+
+    test = (c_char_p * 2)(cast(create_string_buffer(64), c_char_p), cast(create_string_buffer(64), c_char_p))
+    test[0] = b"loadfile"
+    test[1] = b"ytdl://PLfwcn8kB8EmMQSt88kswhY-QqJtWfVYEr"
+
+    command(mpv_handle, test)
+
+    def check_shutdown():
+        while True:
+            off = wait_event(mpv_client_handle, -1)
+            x = c_int.from_address(off)
+            off += sizeof(c_int)
+            y = c_int.from_address(off)
+            off += sizeof(c_int)
+            z = c_int64.from_address(off)
+            off += sizeof(c_uint64)
+            a = c_void_p.from_address(off)
+            print(off, x.value, y.value, z.value, a.value)
+            if x == 1:
+                break
+
+    th = Thread(target=check_shutdown)
+
+    th.start()
+
+    while th.is_alive():
+        try:
+            interact(banner="Mpv Player", local=locals(), exitmsg="Exit")
+        except SystemExit as ex:
+            if ex.code:
+                break
+        except Exception as e:
+            print(e)
+        wakeup(mpv_client_handle)
+
+    command(mpv_handle, (c_char_p * 1)(c_char_p(b"quit")))
+
+    th.join(3)
+
+    destroy(mpv_client_handle)
+
+    terminate_destroy(mpv_handle)
+
+
+if __name__ == '__main__':
+    main()
